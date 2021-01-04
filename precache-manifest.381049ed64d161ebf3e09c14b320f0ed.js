7d4ed99b54b2cab330e730e873ba1617ad69c330
max_brenzey <max57725772@gmail.com>
2021-01-04 16:26:44 +0200

    Updates

---
+++ precache-manifest.381049ed64d161ebf3e09c14b320f0ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "302324d56a3e992af7a5f1839255f105",
+    "url": "/React.js-HealfyFood1/index.html"
+  },
+  {
+    "revision": "a38904c9cdac7a38782a",
+    "url": "/React.js-HealfyFood1/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "61340e871a037cc4aeac",
+    "url": "/React.js-HealfyFood1/static/css/main.e4c698c8.chunk.css"
+  },
+  {
+    "revision": "a38904c9cdac7a38782a",
+    "url": "/React.js-HealfyFood1/static/js/2.0f2e4fa1.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/React.js-HealfyFood1/static/js/2.0f2e4fa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61340e871a037cc4aeac",
+    "url": "/React.js-HealfyFood1/static/js/main.373a4d35.chunk.js"
+  },
+  {
+    "revision": "3bddafc0c695f27a4154",
+    "url": "/React.js-HealfyFood1/static/js/runtime-main.31c14214.js"
+  }
+]);
